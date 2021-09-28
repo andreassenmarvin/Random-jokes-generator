63adf843c8aaf155b64abac7e3ecdd5df8829125
andreassenmarvin <marvinandreassen@gmail.com>
2021-09-28 18:20:50 +0300

    Initial commit

--- app.js
+++ app.js
@@ -5,11 +5,12 @@ document.addEventListener('DOMContentLoaded', getJoke)
 
 button.addEventListener('click', getJoke);
 
-function getJoke(){
-  fetch('https://icanhazdadjoke.com/', {
-    headers: {
-      'Accept': 'application/json'
-    }})
-    .then(data => data.json())
-    .then(obj => jokeDiv.innerHTML = obj.joke);
+function getJoke() {
+    fetch('https://icanhazdadjoke.com/', {
+            headers: {
+                'Accept': 'application/json'
+            }
+        })
+        .then(data => data.json())
+        .then(obj => jokeDiv.innerHTML = obj.joke);
 }
